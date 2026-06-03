837b190dc92bab9081c0fde861ca81e0db5301d9
SMKM <msantiagovargas2@gmail.com>
2026-06-03 17:24:32 -0600

    feat: add middleware for local testing

--- server/main.py
+++ server/main.py
@@ -2,14 +2,24 @@ from fastapi import FastAPI
 from app.database import engine, Base
 from app.routers import router
 from scalar_fastapi import get_scalar_api_reference
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Simple swap")
 app.include_router(router)
 
-@app.get("/scalar",include_in_schema=False)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/scalar", include_in_schema=False)
 def get_docs_scalar():
     return get_scalar_api_reference(
         openapi_url=app.openapi_url,
         title="Scalar API"
     )
+
+
